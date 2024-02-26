c1a05e9fa890b3c7c0a002234e1883966ff4890b
Roshan10k <149761447+Roshan10k@users.noreply.github.com>
2024-02-26 14:44:54 +0545

    Changing layout of buyerpage

--- Buyerpage.py
+++ Buyerpage.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
-# from tkmacosx import Button
+from tkmacosx import Button
 from datetime import datetime
 
 
@@ -12,26 +12,30 @@ root.geometry("1440x1080")
 
 root.configure(bg="gray")
 
-bg_image = ImageTk.PhotoImage(Image.open("Images/login.jpg"))
+frame_image=ImageTk.PhotoImage(Image.open("Images/login.jpg"))
+
+# Background image
+bg_image = PhotoImage(file="Images/mainpage.png")  
+bg_label = Label(root, image=bg_image)
+bg_label.place(relwidth=1, relheight=1)
 
 
-
-#creating a heading
-Heading=LabelFrame(root,bd=2,relief="flat",bg="light yellow")
-Heading.place(x=0,y=0,width=1440,height=55)
-image_logo=ImageTk.PhotoImage(Image.open("Images/logo.png"))
-heading_bg=Label(Heading, image=bg_image).place(relheight=1,relwidth=1)
-name=Label(Heading,text="Luga Pasal",font=("Comic Sans MS",28,"bold"),fg="black").grid(row=0,column=1)
-tagline=Label(Heading,text="Be fashionable!",font="magneto 16 italic",fg="Red").grid(row=0,column=2,padx=280)
+# #creating a heading
+# Heading=LabelFrame(root,bd=2,relief="flat",bg="light yellow")
+# Heading.place(x=0,y=0,width=1440,height=55)
+# image_logo=ImageTk.PhotoImage(Image.open("Images/logo.png"))
+# heading_bg=Label(Heading, image=bg_image).place(relheight=1,relwidth=1)
+# name=Label(Heading,text="Luga Pasal",font=("Comic Sans MS",28,"bold"),fg="black").grid(row=0,column=1)
+# tagline=Label(Heading,text="Be fashionable!",font="magneto 16 italic",fg="Red").grid(row=0,column=2,padx=280)
 
 #creating frames inside root
 Products_frame=LabelFrame(root,bd=2,relief="flat",text="Products",font="arial 16 bold",fg="Red")
-Products_frame.place(x=310,y=55,width=1200,height=720)
+Products_frame.place(x=310,y=80,width=1200,height=720)
 
 
 Button_frame=LabelFrame(root,bd=2,relief="groove")
-Button_frame.place(x=0,y=55,width=310,height=380)
-label_logo_button=Label(Button_frame,image=bg_image,bd=2).place(relheight=1,relwidth=1)
+Button_frame.place(x=0,y=80,width=310,height=350)
+label_logo_button=Label(Button_frame,image=frame_image,bd=2).place(relheight=1,relwidth=1)
 
 
 
