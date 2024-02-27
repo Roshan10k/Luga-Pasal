62cb3731094d406cb337190a398eef7105c4838c
nishadmalla <149762693+nishadmalla@users.noreply.github.com>
2024-02-27 21:22:05 +0545

    adding function to welcome page

--- index.py
+++ index.py
@@ -2,7 +2,14 @@ from tkinter import *
 from PIL import Image,ImageTk
 from tkmacosx import Button
 
+def open_seller_login():
+    root.destroy()
+    import loginseller
+   
 
+def open_buyer_login():
+    root.destroy()
+    import loginbuyer
   
 
 root=Tk()
@@ -18,10 +25,10 @@ bg_label.place(relheight=1,relwidth=1)
 
 font_style = ("Pacifico", 16)
 
-Seller_button=Button(root,text="Seller",bg="violet",font=font_style,relief="groove")
+Seller_button=Button(root,text="Seller",bg="Lightblue",font=font_style,relief="groove",command=open_seller_login)
 Seller_button.place(x=910,y=310,height=60,width=100)
 
-buyer_button=Button(root,text="Buyer",bg="Lightblue",font=font_style,relief="groove")
+buyer_button=Button(root,text="Buyer",bg="Lightblue",font=font_style,relief="groove",command=open_buyer_login)
 buyer_button.place(x=910,y=480,height=60,width=100)
 
 root.mainloop()
