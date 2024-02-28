2e053e7bc94da14c40f97052d9ff579dae3395b5
nishadmalla <149762693+nishadmalla@users.noreply.github.com>
2024-02-28 16:14:38 +0545

    made a frame for seller page

---
+++ sellerpage.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox
+from PIL import Image, ImageTk
+import cv2
+import requests
+from io import BytesIO
+
+class PhotoApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Seller Page")
+        self.root.geometry("1440x800")  # Set the root geometry to 1440x800
+        self.root.minsize(1440, 800)    # Set the minimum window size
+        self.root.maxsize(1440, 800)# Make the window resizable
+
+        # Load b
+
+        
+
+        self.setup_gui()
+
+    def setup_gui(self):
+        # Create and configure frame for form elements
+        frame = ttk.Frame(self.root, padding="40")  # Increased padding for a larger frame
+        frame.place(relx=0.325, rely=0.6, anchor=tk.W)  # Adjusted placement for a more centered position
+        frame.columnconfigure(0, weight=1)
+
+       
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PhotoApp(root)
+    root.mainloop()
